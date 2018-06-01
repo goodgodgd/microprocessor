0223bc492e7c18e0404ebcaf0f02d942d01d18c1
HyukdooChoi <goodgodgd@yonsei.ac.kr>
2018-06-01 10:05:50 +0900

    Update installer.sh

--- hellomcu/installer.sh
+++ hellomcu/installer.sh
@@ -2,20 +2,26 @@
 echo Today is `date`
 echo Intallation starts now!
 
-echo First, install gcc
+echo -e "\nStep1: install gcc"
 sudo apt update; sudo apt install gcc -y
+read -p "Press [enter] to continue"
 
-echo Second, download hellomcu.c
+echo -e "\nStep2: download hellomcu.c and built it"
 mkdir src || (rm -r src; mkdir src)
-wget https://raw.githubusercontent.com/goodgodgd/microprocessor/master/hellomcu1/hellomcu.c -O src/hellomcu.c
-gcc -o hellomcu1 hellomcu.c
-chmod u+x hellomcu
+wget https://raw.githubusercontent.com/goodgodgd/microprocessor/master/hellomcu/hellomcu.c -O src/hellomcu.c
+gcc -o src/hellomcu src/hellomcu.c
+chmod u+x src/hellomcu
+read -p "Press [enter] to continue"
 
-echo Third, move hellomcu to mybin and add mybin to PATH
+echo -e "\nStep3: move hellomcu to mybin try to execute it"
 mkdir ~/mybin
 cp src/hellomcu ~/mybin
-echo excute hellomcu here: "`pwd`"
+echo excute hellomcu at "`pwd`"
 hellomcu
+read -p "Press [enter] to continue"
+
+echo -e "\nStep4: add mybin to PATH and execute it again"
+OLDPATH=$PATH
 export PATH=~/mybin:$PATH
-echo excute hellomcu at "`pwd`" after add mybin to 'PATH'
 hellomcu
+export PATH=$OLDPATH
